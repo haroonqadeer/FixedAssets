f5ceca7cf51754b44456c7d63acfaf33f138024f
haroonqadeer <haroon.qadeer@hotmail.com>
2020-06-26 18:40:26 +0500

    login page changings

--- src/app/pages/fixAsset/asset-entry/asset-entry.component.ts
+++ src/app/pages/fixAsset/asset-entry/asset-entry.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app-asset-entry',
-  templateUrl: './asset-entry.component.html',
-  styleUrls: ['./asset-entry.component.scss']
+  selector: "app-asset-entry",
+  templateUrl: "./asset-entry.component.html",
+  styleUrls: ["./asset-entry.component.scss"],
 })
 export class AssetEntryComponent implements OnInit {
+  editMode = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  editLocation() {
+    if (this.editMode == true) {
+      this.editMode = false;
+    } else {
+      this.editMode = true;
+    }
   }
-
 }
