05d8ee3b76a0ebab712f275bf43913935a3cd927
nabeelfarman <knabeel380@gmail.com>
2020-07-05 23:02:00 +0500

    changes

--- src/app/pages/fixAsset/dashboardy/dashboardy.component.ts
+++ src/app/pages/fixAsset/dashboardy/dashboardy.component.ts
@@ -23,6 +23,8 @@ export class DashboardyComponent implements OnInit {
   allTags = "";
   cmbChartLocation = "";
   cmbTblLocation = "";
+  cmbLocation = "";
+  cmbOfcType = "";
 
   searchChartLocation = "";
   searchTblLocation = "";
@@ -31,6 +33,8 @@ export class DashboardyComponent implements OnInit {
   searchTagOfc = "";
   searchTagLoc = "";
 
+  ofcTypeList = [];
+  assetDetailList = [];
   tempLocList = [];
   tempTagList = [];
   allLocDetList = [];
@@ -56,6 +60,83 @@ export class DashboardyComponent implements OnInit {
     this.getTagDetail();
     this.getTagDateWise();
     this.getLocation();
+    this.getAssetDetail();
+    this.getOfficeType();
+  }
+
+  getOfficeType() {
+    var reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      // Authorization: "Bearer " + Token,
+    });
+
+    this.http
+      .get(this.serverUrl + "getofctype", { headers: reqHeader })
+      .subscribe((data: any) => {
+        this.ofcTypeList = data;
+      });
+  }
+
+  getAssetDetail() {
+    var reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      // Authorization: "Bearer " + Token,
+    });
+
+    this.http
+      .get(this.serverUrl + "getassetdetail", { headers: reqHeader })
+      .subscribe((data: any) => {
+        this.assetDetailList = data;
+      });
+  }
+
+  getAssetDetailLocOfcType() {
+    if (this.cmbLocation != "" && this.cmbOfcType != "") {
+      var reqHeader = new HttpHeaders({
+        "Content-Type": "application/json",
+        // Authorization: "Bearer " + Token,
+      });
+
+      this.http
+        .get(
+          this.serverUrl +
+            "getassetdetail?UserId=0&SubLocID=" +
+            this.cmbLocation +
+            "&OfficeTypeID=" +
+            this.cmbOfcType,
+          { headers: reqHeader }
+        )
+        .subscribe((data: any) => {
+          this.assetDetailList = data;
+        });
+    } else if (this.cmbLocation != "" && this.cmbOfcType == "") {
+      var reqHeader = new HttpHeaders({
+        "Content-Type": "application/json",
+        // Authorization: "Bearer " + Token,
+      });
+
+      this.http
+        .get(this.serverUrl + "getassetdetail?SubLocID=" + this.cmbLocation, {
+          headers: reqHeader,
+        })
+        .subscribe((data: any) => {
+          this.assetDetailList = data;
+        });
+    } else if (this.cmbLocation == "" && this.cmbOfcType != "") {
+      var reqHeader = new HttpHeaders({
+        "Content-Type": "application/json",
+        // Authorization: "Bearer " + Token,
+      });
+
+      this.http
+        .get(
+          this.serverUrl + "getassetdetail?OfficeTypeID=" + this.cmbOfcType,
+          { headers: reqHeader }
+        )
+        .subscribe((data: any) => {
+          this.assetDetailList = data;
+        });
+    }
   }
 
   editLocationModalTitle(text) {
@@ -72,173 +153,360 @@ export class DashboardyComponent implements OnInit {
   }
 
   testChart() {
-    let chart = new Chart({
-      chart: {
-        type: "column",
-      },
-      title: {
-        text: "Asset by Categories",
-      },
-      subtitle: {
-        text:
-          'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>',
-      },
-      xAxis: {
-        type: "category",
-      },
-      yAxis: {
-        title: {
-          text: "Total percent market share",
-        },
-      },
-      legend: {
-        enabled: false,
-      },
-      plotOptions: {
-        series: {
-          dataLabels: {
-            enabled: true,
-            format: "{point.y:.1f}%",
-          },
-        },
-      },
-
-      tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat:
-          '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>',
-      },
-
-      series: [
-        {
-          name: "Browsers",
-          data: [
-            {
-              name: "Chrome",
-              y: 62.74,
-              drilldown: "Chrome",
-            },
-            {
-              name: "Firefox",
-              y: 10.57,
-              drilldown: "Firefox",
-            },
-            {
-              name: "Internet Explorer",
-              y: 7.23,
-              drilldown: "Internet Explorer",
-            },
-            {
-              name: "Safari",
-              y: 5.58,
-              drilldown: "Safari",
-            },
-            {
-              name: "Edge",
-              y: 4.02,
-              drilldown: "Edge",
-            },
-            {
-              name: "Opera",
-              y: 1.92,
-              drilldown: "Opera",
-            },
-            {
-              name: "Other",
-              y: 7.62,
-              drilldown: null,
-            },
-          ],
-        },
-      ],
-      drilldown: {
-        series: [
-          {
-            name: "Chrome",
-            id: "Chrome",
-            data: [
-              ["v65.0", 0.1],
-              ["v64.0", 1.3],
-              ["v63.0", 53.02],
-              ["v62.0", 1.4],
-              ["v61.0", 0.88],
-              ["v60.0", 0.56],
-              ["v59.0", 0.45],
-              ["v58.0", 0.49],
-              ["v57.0", 0.32],
-              ["v56.0", 0.29],
-              ["v55.0", 0.79],
-              ["v54.0", 0.18],
-              ["v51.0", 0.13],
-              ["v49.0", 2.16],
-              ["v48.0", 0.13],
-              ["v47.0", 0.11],
-              ["v43.0", 0.17],
-              ["v29.0", 0.26],
-            ],
-          },
-          {
-            name: "Firefox",
-            id: "Firefox",
-            data: [
-              ["v58.0", 1.02],
-              ["v57.0", 7.36],
-              ["v56.0", 0.35],
-              ["v55.0", 0.11],
-              ["v54.0", 0.1],
-              ["v52.0", 0.95],
-              ["v51.0", 0.15],
-              ["v50.0", 0.1],
-              ["v48.0", 0.31],
-              ["v47.0", 0.12],
-            ],
-          },
-          {
-            name: "Internet Explorer",
-            id: "Internet Explorer",
-            data: [
-              ["v11.0", 6.2],
-              ["v10.0", 0.29],
-              ["v9.0", 0.27],
-              ["v8.0", 0.47],
-            ],
-          },
-          {
-            name: "Safari",
-            id: "Safari",
-            data: [
-              ["v11.0", 3.39],
-              ["v10.1", 0.96],
-              ["v10.0", 0.36],
-              ["v9.1", 0.54],
-              ["v9.0", 0.13],
-              ["v5.1", 0.2],
-            ],
-          },
-          {
-            name: "Edge",
-            id: "Edge",
-            data: [
-              ["v16", 2.6],
-              ["v15", 0.92],
-              ["v14", 0.4],
-              ["v13", 0.1],
-            ],
-          },
-          {
-            name: "Opera",
-            id: "Opera",
-            data: [
-              ["v50.0", 0.96],
-              ["v49.0", 0.82],
-              ["v12.1", 0.14],
-            ],
-          },
-        ],
-      },
+    var reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      // Authorization: "Bearer " + Token,
     });
 
-    this.test_chart = chart;
+    this.http
+      .get(this.serverUrl + "getallassetdashboard", { headers: reqHeader })
+      .subscribe((data: any) => {
+        let chart = new Chart({
+          chart: {
+            type: "column",
+          },
+          title: {
+            text: "Asset by Categories",
+          },
+          xAxis: {
+            type: "category",
+          },
+          yAxis: {
+            title: {
+              text: "Total Number of Assets",
+            },
+          },
+          legend: {
+            enabled: false,
+          },
+          plotOptions: {
+            series: {
+              dataLabels: {
+                enabled: true,
+                format: "{point.y:.1f}%",
+              },
+            },
+          },
+
+          // tooltip: {
+          //   headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          //   pointFormat:
+          //     '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>',
+          // },
+
+          series: [
+            {
+              name: data[0].totalTagsCreated,
+              data: [
+                {
+                  name: "Computers",
+                  y: data[0].computers,
+                  // drilldown: "Chrome",
+                },
+                {
+                  name: "Drawing Equipment",
+                  y: data[0].drawingequipment,
+                  // drilldown: "Firefox",
+                },
+                {
+                  name: "Electric Items",
+                  y: data[0].electricitems,
+                  // drilldown: "Internet Explorer",
+                },
+                {
+                  name: "Furnitures",
+                  y: data[0].furnitures,
+                  // drilldown: "Safari",
+                },
+                {
+                  name: "Office Equipements",
+                  y: data[0].officeequipments,
+                  // drilldown: "Edge",
+                },
+                {
+                  name: "Plants",
+                  y: data[0].plants,
+                  // drilldown: "Opera",
+                },
+                {
+                  name: "Vehicles",
+                  y: data[0].vehicles,
+                  // drilldown: null,
+                },
+              ],
+            },
+          ],
+          // drilldown: {
+          //   series: [
+          //     {
+          //       name: "Chrome",
+          //       id: "Chrome",
+          //       data: [
+          //         ["v65.0", 0.1],
+          //         ["v64.0", 1.3],
+          //         ["v63.0", 53.02],
+          //         ["v62.0", 1.4],
+          //         ["v61.0", 0.88],
+          //         ["v60.0", 0.56],
+          //         ["v59.0", 0.45],
+          //         ["v58.0", 0.49],
+          //         ["v57.0", 0.32],
+          //         ["v56.0", 0.29],
+          //         ["v55.0", 0.79],
+          //         ["v54.0", 0.18],
+          //         ["v51.0", 0.13],
+          //         ["v49.0", 2.16],
+          //         ["v48.0", 0.13],
+          //         ["v47.0", 0.11],
+          //         ["v43.0", 0.17],
+          //         ["v29.0", 0.26],
+          //       ],
+          //     },
+          //     {
+          //       name: "Firefox",
+          //       id: "Firefox",
+          //       data: [
+          //         ["v58.0", 1.02],
+          //         ["v57.0", 7.36],
+          //         ["v56.0", 0.35],
+          //         ["v55.0", 0.11],
+          //         ["v54.0", 0.1],
+          //         ["v52.0", 0.95],
+          //         ["v51.0", 0.15],
+          //         ["v50.0", 0.1],
+          //         ["v48.0", 0.31],
+          //         ["v47.0", 0.12],
+          //       ],
+          //     },
+          //     {
+          //       name: "Internet Explorer",
+          //       id: "Internet Explorer",
+          //       data: [
+          //         ["v11.0", 6.2],
+          //         ["v10.0", 0.29],
+          //         ["v9.0", 0.27],
+          //         ["v8.0", 0.47],
+          //       ],
+          //     },
+          //     {
+          //       name: "Safari",
+          //       id: "Safari",
+          //       data: [
+          //         ["v11.0", 3.39],
+          //         ["v10.1", 0.96],
+          //         ["v10.0", 0.36],
+          //         ["v9.1", 0.54],
+          //         ["v9.0", 0.13],
+          //         ["v5.1", 0.2],
+          //       ],
+          //     },
+          //     {
+          //       name: "Edge",
+          //       id: "Edge",
+          //       data: [
+          //         ["v16", 2.6],
+          //         ["v15", 0.92],
+          //         ["v14", 0.4],
+          //         ["v13", 0.1],
+          //       ],
+          //     },
+          //     {
+          //       name: "Opera",
+          //       id: "Opera",
+          //       data: [
+          //         ["v50.0", 0.96],
+          //         ["v49.0", 0.82],
+          //         ["v12.1", 0.14],
+          //       ],
+          //     },
+          //   ],
+          // },
+        });
+
+        this.test_chart = chart;
+      });
+  }
+
+  getChartLocationWise() {
+    var reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      // Authorization: "Bearer " + Token,
+    });
+
+    this.http
+      .get(
+        this.serverUrl +
+          "getallassetlocwisedashboard?LocationID=" +
+          this.cmbChartLocation,
+        {
+          headers: reqHeader,
+        }
+      )
+      .subscribe((data: any) => {
+        let chart = new Chart({
+          chart: {
+            type: "column",
+          },
+          title: {
+            text: "Asset by Categories",
+          },
+          xAxis: {
+            type: "category",
+          },
+          yAxis: {
+            title: {
+              text: "Total Number of Assets",
+            },
+          },
+          legend: {
+            enabled: false,
+          },
+          plotOptions: {
+            series: {
+              dataLabels: {
+                enabled: true,
+                format: "{point.y:.1f}%",
+              },
+            },
+          },
+
+          // tooltip: {
+          //   headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          //   pointFormat:
+          //     '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>',
+          // },
+
+          series: [
+            {
+              name: data[0].totalTagsCreated,
+              data: [
+                {
+                  name: "Computers",
+                  y: data[0].computers,
+                  // drilldown: "Chrome",
+                },
+                {
+                  name: "Drawing Equipment",
+                  y: data[0].drawingequipment,
+                  // drilldown: "Firefox",
+                },
+                {
+                  name: "Electric Items",
+                  y: data[0].electricitems,
+                  // drilldown: "Internet Explorer",
+                },
+                {
+                  name: "Furnitures",
+                  y: data[0].furnitures,
+                  // drilldown: "Safari",
+                },
+                {
+                  name: "Office Equipements",
+                  y: data[0].officeequipments,
+                  // drilldown: "Edge",
+                },
+                {
+                  name: "Plants",
+                  y: data[0].plants,
+                  // drilldown: "Opera",
+                },
+                {
+                  name: "Vehicles",
+                  y: data[0].vehicles,
+                  // drilldown: null,
+                },
+              ],
+            },
+          ],
+          // drilldown: {
+          //   series: [
+          //     {
+          //       name: "Chrome",
+          //       id: "Chrome",
+          //       data: [
+          //         ["v65.0", 0.1],
+          //         ["v64.0", 1.3],
+          //         ["v63.0", 53.02],
+          //         ["v62.0", 1.4],
+          //         ["v61.0", 0.88],
+          //         ["v60.0", 0.56],
+          //         ["v59.0", 0.45],
+          //         ["v58.0", 0.49],
+          //         ["v57.0", 0.32],
+          //         ["v56.0", 0.29],
+          //         ["v55.0", 0.79],
+          //         ["v54.0", 0.18],
+          //         ["v51.0", 0.13],
+          //         ["v49.0", 2.16],
+          //         ["v48.0", 0.13],
+          //         ["v47.0", 0.11],
+          //         ["v43.0", 0.17],
+          //         ["v29.0", 0.26],
+          //       ],
+          //     },
+          //     {
+          //       name: "Firefox",
+          //       id: "Firefox",
+          //       data: [
+          //         ["v58.0", 1.02],
+          //         ["v57.0", 7.36],
+          //         ["v56.0", 0.35],
+          //         ["v55.0", 0.11],
+          //         ["v54.0", 0.1],
+          //         ["v52.0", 0.95],
+          //         ["v51.0", 0.15],
+          //         ["v50.0", 0.1],
+          //         ["v48.0", 0.31],
+          //         ["v47.0", 0.12],
+          //       ],
+          //     },
+          //     {
+          //       name: "Internet Explorer",
+          //       id: "Internet Explorer",
+          //       data: [
+          //         ["v11.0", 6.2],
+          //         ["v10.0", 0.29],
+          //         ["v9.0", 0.27],
+          //         ["v8.0", 0.47],
+          //       ],
+          //     },
+          //     {
+          //       name: "Safari",
+          //       id: "Safari",
+          //       data: [
+          //         ["v11.0", 3.39],
+          //         ["v10.1", 0.96],
+          //         ["v10.0", 0.36],
+          //         ["v9.1", 0.54],
+          //         ["v9.0", 0.13],
+          //         ["v5.1", 0.2],
+          //       ],
+          //     },
+          //     {
+          //       name: "Edge",
+          //       id: "Edge",
+          //       data: [
+          //         ["v16", 2.6],
+          //         ["v15", 0.92],
+          //         ["v14", 0.4],
+          //         ["v13", 0.1],
+          //       ],
+          //     },
+          //     {
+          //       name: "Opera",
+          //       id: "Opera",
+          //       data: [
+          //         ["v50.0", 0.96],
+          //         ["v49.0", 0.82],
+          //         ["v12.1", 0.14],
+          //       ],
+          //     },
+          //   ],
+          // },
+        });
+
+        this.test_chart = chart;
+      });
   }
 
   getTagDateWise() {
