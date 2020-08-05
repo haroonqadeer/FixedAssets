8ccb3b6e5f5938f049318ecb5c5f7291ca8f5de4
AdnanAli7 <adnanali1990@gmail.com>
2020-08-06 00:27:19 +0500

    ...

--- src/app/pages/fixAsset/bridges/bridges.component.ts
+++ src/app/pages/fixAsset/bridges/bridges.component.ts
@@ -24,8 +24,8 @@ export class BridgesComponent implements OnInit {
     loadingBar = true;
     reqType = '';
     reqStatus = false;
-    //serverUrl = "http://95.217.206.195:2007/api/";
-    serverUrl = "http://localhost:12345/api/";
+    serverUrl = "http://95.217.206.195:2007/api/";
+    //serverUrl = "http://localhost:12345/api/";
 
     toggleView = "form";
     
