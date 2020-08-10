c4bec104624a67b3db9f0dc6b977c4d16728411b
haroonqadeer <haroon.qadeer@hotmail.com>
2020-08-10 15:17:40 +0500

    table dragger

--- src/app/pages/fixAsset/Reports/asset-register-rpt/asset-register-rpt.component.ts
+++ src/app/pages/fixAsset/Reports/asset-register-rpt/asset-register-rpt.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
+
+import tableDragger from "table-dragger";
 
 @Component({
-  selector: 'app-asset-register-rpt',
-  templateUrl: './asset-register-rpt.component.html',
-  styleUrls: ['./asset-register-rpt.component.scss']
+  selector: "app-asset-register-rpt",
+  templateUrl: "./asset-register-rpt.component.html",
+  styleUrls: ["./asset-register-rpt.component.scss"],
 })
 export class AssetRegisterRptComponent implements OnInit {
+  // serverUrl = "http://95.217.206.195:2007/api/";
+  //serverUrl = "http://localhost:12345/api/";
 
-  constructor() { }
+  serverUrl = "http://localhost:6090/api/";
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-  }
+    var el = document.getElementById("table");
 
+    var dragger = tableDragger(el, {
+      mode: "row",
+      dragHandler: ".drag",
+      onlyBody: true,
+      animation: 300,
+    });
+    // tableDragger(document.querySelector("#table"));
+  }
 }
