49e9390843e782bc424f6bdc8d05f22972432603
haroonqadeer <haroon.qadeer@hotmail.com>
2020-08-11 10:28:55 +0500

    asset register report

--- src/app/pages/fixAsset/Reports/asset-register-rpt/asset-register-rpt.component.ts
+++ src/app/pages/fixAsset/Reports/asset-register-rpt/asset-register-rpt.component.ts
@@ -4,8 +4,22 @@ import {
   HttpHeaders,
   HttpErrorResponse,
 } from "@angular/common/http";
+import { AppComponent } from "src/app/app.component";
 
 // import tableDragger from "../../../../../../node_modules/table-dragger/src/index.js";
+
+const ELEMENT_DATA = [
+  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
+  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
+  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
+  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
+  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
+  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
+  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
+  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
+  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
+  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
+];
 
 @Component({
   selector: "app-asset-register-rpt",
@@ -18,7 +32,10 @@ export class AssetRegisterRptComponent implements OnInit {
 
   // serverUrl = "http://localhost:6090/api/";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private app: AppComponent) {}
+
+  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
     setTimeout(() => this.getTableDrag(), 500);
@@ -31,5 +48,9 @@ export class AssetRegisterRptComponent implements OnInit {
     //     // onlyBody: true,
     //     animation: 300,
     //   });
+  }
+
+  printDiv() {
+    this.app.printReport("#myTable");
   }
 }
