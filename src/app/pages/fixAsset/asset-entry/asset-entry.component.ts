71a2115fa501ed4c738beb640827342c583c88a6
haroonqadeer <haroon.qadeer@hotmail.com>
2020-09-07 10:10:41 +0500

    asset register report setting

--- src/app/pages/fixAsset/asset-entry/asset-entry.component.ts
+++ src/app/pages/fixAsset/asset-entry/asset-entry.component.ts
@@ -561,8 +561,7 @@ export class AssetEntryComponent implements OnInit {
     this.rdbTransMode = "Sender";
     this.rdbTransRptMode = "1";
     this.rdbAsset = "1";
-    this.disableOfcType = true;
-    // this.getAssetDetail();
+    this.disableOfcType = true;    
     this.getTags();
     this.getLocation();
     this.getTransLocation();
